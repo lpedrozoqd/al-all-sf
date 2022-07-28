2baf0151af5e678f9fcc0666b734b3e574ef11c1
Leonel Pedrozo Quiroz <leonel.pedrozo.quiroz.d@gmail.com>
2022-07-27 23:25:30 -0500

    Uso de parameter-named

--- src/main/java/edu/lpq/alallsf/repository/UserRepository.java
+++ src/main/java/edu/lpq/alallsf/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package edu.lpq.alallsf.repository;
 
+import edu.lpq.alallsf.dto.UserDTO;
 import edu.lpq.alallsf.entity.Users;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -34,8 +36,16 @@ public interface UserRepository extends JpaRepository<Users,Long> {
 
     List<Users> findByNameContainingOrderByIdDesc(String name);
 
+   
+    @Query("SELECT new edu.lpq.alallsf.dto.UserDTO(u.id, u.name, u.birthDate)" +
+        " FROM Users u" +
+        " where u.birthDate=:parameterFecha " +
+        " and u.email=:parameterEmail ")
+    Optional<UserDTO> getAllByBirthDateAndEmail(@Param("parameterFecha") LocalDate fechaInicio,
+        @Param("parameterEmail") String email); 
 
-    
+        
+     
 
 
 
